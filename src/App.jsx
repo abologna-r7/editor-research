4f8353eaceeead7169f9941595d010469911bc26
abologna-r7 <antonio_bologna@rapid7.com>
2018-11-15 17:24:26 -0700

    initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style/base.scss';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack')
+
+const outputDirectory = 'dist';
+
+module.exports = {
+  entry: ['babel-polyfill', './src/index.jsx'],
+  mode: 'development',
+  devtool: '#cheap-module-source-map',
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'resolve-url-loader',
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+              includePaths: [
+                path.resolve(__dirname, 'node_modules'),
+                path.resolve(__dirname, 'src', 'style'),
+              ],
+            },
+          },
+        ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+  resolve: {
+    unsafeCache: true,
+    modules: ['./src/scripts', 'node_modules'],
+    extensions: [
+      '.js',
+      '.jsx',
+      '.json',
+      '.html',
+      '.css',
+      '.scss',
+    ],
+    alias: {
+      'react': path.join(__dirname, 'node_modules', 'react'),
+    },
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+    inline: true,
+    contentBase: path.resolve(__dirname, "dist"),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "bundle.css",
+      chunkFilename: "bundle.css"
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+  ]
+};
